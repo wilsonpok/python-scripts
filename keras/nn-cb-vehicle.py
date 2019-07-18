bdbcbc76090bb944e9b7af6581dd61d1e295f1bd
Wilson Pok <wilson.pok@ambiata.com>
2019-07-18 15:10:26 +1000

    python 3 version of csv2vw

--- keras/nn-cb-vehicle.py
+++ keras/nn-cb-vehicle.py
@@ -2,9 +2,12 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from pathlib import Path
 from keras import models
 from keras import layers
+from keras.utils.np_utils import to_categorical
+from sklearn.model_selection import train_test_split
 
 
 home = str(Path.home())
@@ -19,20 +22,46 @@ input_file = home + '/vehicle-dataset/dat_as_cb_loss_only.csv'
 
 df = pd.read_csv(input_file)
 
-
 df.shape
 # (846, 23)
 
 
 
+######################
+# Prepare data
+######################
+
 x_cols = [col for col in df if col.startswith('X')]
-
-df['Y']
-
 
 df_x = df.loc[:, x_cols]
 df_x.shape
 # (846, 18)
+
+nn_input_x = np.asarray(df_x)
+
+
+
+
+df['Y_code'] = None
+df['Y_code'].loc[df.Y == 'opel'] = 0
+df['Y_code'].loc[df.Y == 'saab'] = 1
+df['Y_code'].loc[df.Y == 'bus'] = 2
+df['Y_code'].loc[df.Y == 'van'] = 3
+
+nn_input_y = to_categorical(df['Y_code'])
+
+
+
+
+
+######################
+# Split data
+######################
+
+nn_input_x_train, nn_input_x_test, nn_input_y_train, nn_input_y_test = train_test_split(nn_input_x, nn_input_y, test_size=0.2, random_state=666)
+
+[x.shape for x in [nn_input_x_train, nn_input_x_test, nn_input_y_train, nn_input_y_test]]
+# [(676, 18), (170, 18), (676, 4), (170, 4)]
 
 
 
@@ -51,7 +80,37 @@ model.compile(optimizer='rmsprop',
 
 
 
-history = model.fit(np.asarray(df_x),
-                    np.asarray(df['Y']),
+
+######################
+# Train model
+######################
+
+history = model.fit(nn_input_x_train,
+                    nn_input_y_train,
                     epochs=20,
                     batch_size=16)
+
+
+
+######################
+# Plot results
+######################
+
+plt.clf()
+loss = history.history['loss']
+# val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+
+######################
+# Predict test data
+######################
+
+model.predict(nn_input_x_test)
